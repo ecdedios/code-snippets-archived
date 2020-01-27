9241165e2172fe9de170fa2e60d60724590f037d
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2020-01-26 21:36:10 -0600

    add headers

--- nlp.py
+++ nlp.py
@@ -1,3 +1,7 @@
+# *************************************************
+# HEADER
+# *************************************************
+
 # -*- coding: utf-8 -*-
 """
 
@@ -24,7 +28,9 @@ DON'T FORGET TO:
 
 
 
-
+# *************************************************
+# ENVIRONMENT
+# *************************************************
 
 # for reading files from the local machine
 import os
@@ -59,6 +65,10 @@ pd.set_option('display.max_rows', None)
 
 
 
+# *************************************************
+# BASIC CLEAN
+# *************************************************
+
 def clean(text):
   """
   A simple function to clean up the data. All the words that
@@ -73,3 +83,69 @@ def clean(text):
     .lower())
   words = re.sub(r'[^\w\s]', '', text).split()
   return [wnl.lemmatize(word) for word in words if word not in stopwords]
+
+
+
+# *************************************************
+# DISPLAY MISSING VALUES
+# *************************************************
+
+def missing_values_col(df):
+    """
+    Write or use a previously written function to return the
+    total missing values and the percent missing values by column.
+    """
+    null_count = df.isnull().sum()
+    null_percentage = (null_count / df.shape[0]) * 100
+    empty_count = pd.Series(((df == ' ') | (df == '')).sum())
+    empty_percentage = (empty_count / df.shape[0]) * 100
+    nan_count = pd.Series(((df == 'nan') | (df == 'NaN')).sum())
+    nan_percentage = (nan_count / df.shape[0]) * 100
+    return pd.DataFrame({'num_missing': null_count, 'missing_percentage': null_percentage,
+                         'num_empty': empty_count, 'empty_percentage': empty_percentage,
+                         'nan_count': nan_count, 'nan_percentage': nan_percentage})
+
+
+
+# *************************************************
+# READ CSV FILE
+# *************************************************
+
+df = pd.read_csv('../data/campaign_nlp.csv')
+
+
+
+# *************************************************
+# READ EXCEL FILE
+# *************************************************
+
+df = pd.read_excel('../data/campaign_nlp.xlsx')
+
+
+
+# *************************************************
+# READ ALL FILES WITHIN A FOLDER
+# *************************************************
+
+def read_data(input_folder):
+    '''
+    This function reads each the raw data files as dataframes and
+    combines them into a single data frame.
+    '''
+    for i, file_name in enumerate(os.listdir(input_folder)):
+        try:
+            # df = pd.read_excel(os.path.join(input_folder, file_name))
+            df = pd.read_csv(os.path.join(input_folder, file_name))
+            df['file_name'] = file_name
+            if i == 0:
+                final_df = df.copy()
+            else:
+                final_df = final_df.append(df)
+
+        except Exception as e:
+            print(f"Cannot read file: {file_name}")
+            print(str(e))
+    return final_df
+
+input_folder = 'G:/path/to/data/parent_folder_name'
+input_df = read_data(input_folder)
